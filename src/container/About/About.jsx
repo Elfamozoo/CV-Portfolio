0429ec66b5cf9c15ece979eee3b2a0d454fe5313
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-06-10 17:34:05 +0200

    Page projets effectué + dark mode inclus mais a revoir

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { images } from "../../constants";
 import { urlFor, client } from "../../client";
+import { AppWrap } from "../../wrapper";
 import "./About.scss";
 
 const About = () => {
@@ -44,4 +44,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "bio");
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -67,4 +67,4 @@ const Header = () => {
   );
 };
 
-export default AppWrap(Header, 'home');
+export default AppWrap(Header, 'accueil');
